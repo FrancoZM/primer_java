0c16f2162f5a40ed09672d50459a5b2190a5b71b
FrancoZM <165206999+FrancoZM@users.noreply.github.com>
2024-05-14 11:41:29 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+var n1 = 20;
+var n2 = 45;
+        
+console.log("Valor de n1:"+ n1);
+console.log("Valor de n2:" + n2);
+
+var suma = n1 + n2;
+console.log("La suma es:"+ suma);
+
+if (n1 > n2){ console.log("El numero mayor es:"+n1)}
+else if (n2>n1) { console.log ("El numero mayor es:"+n2)}
+else {"Son numeros iguales"};
+
+let resultado = (n1>n2)?console.log("el primer numero es mayor"): console.log("Segundo numero es mayor");
+
+
